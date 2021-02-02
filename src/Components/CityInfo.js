bfa8cb1ea237beee25138473ee0a325b477d9f4a
LeonorCardoso <61851452+LeonorCardoso@users.noreply.github.com>
2021-02-02 12:35:12 +0000

    Imported React Animated Weather and changed the sun image to the animated sun icon

--- src/Components/CityInfo.js
+++ src/Components/CityInfo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactAnimatedWeather from 'react-animated-weather';
 import "../CSS_Components/CityInfo.css";
 
 export default function CityInfo() {
@@ -6,7 +7,6 @@ export default function CityInfo() {
     city: "Lisbon",
     temperature: 13,
     description: "Clear sky",
-    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
     humidity: 2,
     wind: 8
   };
@@ -17,10 +17,11 @@ export default function CityInfo() {
         <div className="col-5">
           <h4 id="city">{weatherData.city}</h4>
           <h5 className="weather-temperature">
-            <img
-              src={weatherData.imgUrl}
-              alt={weatherData.description}
-              className="todays-icon"
+            <ReactAnimatedWeather
+              icon="CLEAR_DAY"
+              color="#fbd46d"
+              size={50}
+              animate={true}
             />
             <span id="temperature" className="temperature-number">
               {weatherData.temperature}
