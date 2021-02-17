15f90864e8148a1af5de1f14c640dc4f1a65cfe4
LeonorCardoso <61851452+LeonorCardoso@users.noreply.github.com>
2021-02-17 14:50:10 +0000

    Fixed the issues with SearchWeather

--- src/Components/SearchWeather.js
+++ src/Components/SearchWeather.js
@@ -4,24 +4,23 @@ import axios from "axios";
 import "../CSS_Components/Header.css";
 
 export default function SearchWeather(props) {
-  const [weatherData, setWeatherData] = useState({ ready: false});
-  const [city , setCity] = useState(props.defaultCity); 
-  
-    function handleResponse(response) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
+  function handleResponse(response) {
     setWeatherData({
       ready: true,
+      description: response.data.weather[0].description,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
-      date: new Date(response.data.dt * 1000),
-      description: response.data.weather[0].description,
-      icon: response.data.weather[0].icon,
       wind: response.data.wind.speed,
       city: response.data.name,
+      date: new Date(response.data.dt * 1000),
+      icon: response.data.weather[0].icon,
     });
   }
 
-
-  function handleSubmit(event){
+  function handleSubmit(event) {
     event.preventDefault();
     search();
   }
@@ -30,28 +29,28 @@ export default function SearchWeather(props) {
     setCity(event.target.value);
   }
 
-  function search(){
+  function search() {
     const apiKey = "58ce3f6986fa88fa9731f046fe82bae3";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
-  }
+  } 
 
   if(weatherData.ready){
   return (
     <div className="Header">
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter a city"
-          autocomplete="off"
-          className="enter-city"
-          onChanege={updateCity}
-        />
-        <input type="submit" value="Search" className="other-city" />
-        <button type="button">
-          <i className="fas fa-map-marker-alt"></i>
-        </button>
-      </form>
+              <input
+                  type="text"
+                  placeholder="Enter a city"
+                  autocomplete="off"
+                  className="enter-city"
+                  onChange={updateCity}
+              />
+              <input type="submit" value="Search" className="other-city" />
+              <button type="button">
+              <i className="fas fa-map-marker-alt"></i>
+              </button>
+            </form>
       <CityInfo data={weatherData} />
     </div> 
   );} else {
