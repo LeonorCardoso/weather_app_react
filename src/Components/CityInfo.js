f40a73cb57bdfd588146e87e7685b164a4eafe06
LeonorCardoso <61851452+LeonorCardoso@users.noreply.github.com>
2021-02-17 14:41:59 +0000

    Fixed some issues with the date and the search engine

--- src/Components/CityInfo.js
+++ src/Components/CityInfo.js
@@ -1,67 +1,45 @@
-import React, { useState }  from "react";
-import ReactAnimatedWeather from 'react-animated-weather';
-import axios from "axios";
+import React from "react";
+import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 import "../CSS_Components/CityInfo.css";
 
-export default function CityInfo() {
-  const [weatherData, setWeatherData] = useState({ ready: false}); 
-  
-
-  function handleResponse(response){
-    setWeatherData({
-      ready: true,
-      city: response.data.name,
-      temperature: response.data.main.temp,
-      description: response.data.weather[0].description,
-      humidity: response.data.main.humidity,
-      wind: response.data.wind.speed,
-      icon: <ReactAnimatedWeather
-                  icon="CLEAR_DAY"
-                  color="#fbd46d"
-                  size={64}
-                  animate={true}
-            />, 
-    });
-  }
-
-  if(weatherData.ready){
+export default function CityInfo(props) {
     return (
-      <div className="city-info">
-        <div className="row justify-content-center">
-          <div className="col-5">
-            <h4 id="city">{weatherData.city}</h4>
-            <h5 className="weather-temperature">
-              <span>{weatherData.icon}</span>
-              <span className="temperature">
-                <span id="temperature" className="temperature-number">
-                  {Math.round(weatherData.temperature)}
+      <div>
+        <span className="current-date"><FormattedDate date={props.data.date} /></span>
+        <br />
+        <br />
+        <div className="city-info">
+          <div className="row justify-content-center">
+            <div className="col-5">
+              <h4 id="city">{props.data.city}</h4>
+              <h5 className="weather-temperature">
+                <WeatherIcon code={props.data.icon} />
+
+                <span className="temperature">
+                  <span id="temperature" className="temperature-number">
+                    {Math.round(props.data.temperature)}
+                  </span>
+                    <span className="units">
+                    <a href="/">°C</a>
+                    <span>|</span>
+                    <a href="/">°F</a>
+                  </span>
                 </span>
-                  <span className="units">
-                  <a href="/">°C</a>
-                  <span>|</span>
-                  <a href="/">°F</a>
-                </span>
-              </span>
-            </h5>
-          </div>
-          <div class="col-5">
-            <h5>
-              Humidity:{weatherData.humidity}%
-              <br />
-              Wind:{Math.round(weatherData.wind)}km/h
-            </h5>
+              </h5>
+            </div>
+            <div class="col-5">
+              <h5>
+                Humidity:{props.data.humidity}%
+                <br />
+                Wind:{Math.round(props.data.wind)}km/h
+              </h5>
+            </div>
           </div>
         </div>
       </div>
     );
-  } else {
-    const apiKey = "58ce3f6986fa88fa9731f046fe82bae3";
-    let city = "Paris";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-
-    return "Loading...";
-  }
+  
 
   
 }
--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,18 +1,12 @@
 import React from "react";
-import Header from "./Components/Header.js";
-import CurrentDate from "./Components/CurrentDate.js";
-import CityInfo from "./Components/CityInfo.js";
+import SearchWeather from "./Components/SearchWeather.js";
 import Forecast from "./Components/Forecast";
 import "./WeatherApp.css";
 
 export default function WeatherApp() {
   return (
     <div className="weather-app">
-      <Header />
-      <CurrentDate />
-      <br />
-      <br />
-      <CityInfo />
+      <SearchWeather defaultCity="Lisbon"/>
       <br />
       <br />
       <Forecast />
